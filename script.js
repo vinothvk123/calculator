bc381b5ee4f0f861393bf648052f4cd894cbcc3f
vinothvk123 <vk30032000@gmail.com>
2023-11-14 22:31:21 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+let outputscreen=document.getElementById("output-screen");
+
+function display(num){
+    outputscreen.value += num;
+}
+function calculate(){
+    try{
+        outputscreen.value = eval(outputscreen.value);
+    }
+    catch(err){
+        alert("invalid");
+    }
+    }
+    function clearscreen(){
+        document.getElementById("output-screen").value=" ";
+    }
+    function del(){
+    outputscreen.value=outputscreen.value.slice(0,-1);
+}
+
